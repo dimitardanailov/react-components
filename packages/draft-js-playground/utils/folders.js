ba43470d0ce3b750c0618d4f5946804071d77d87
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-03 20:39:26 +0200

    package has a simple util helper.

---
+++ packages/draft-js-playground/utils/folders.js
@@ -0,0 +1,6 @@
+const path = require('path')
+
+const BASE_DIR = __dirname.replace('/utils', '')
+
+module.exports.APP_DIR = path.resolve(BASE_DIR, 'app')
+module.exports.DIST_DIR = path.resolve(BASE_DIR, 'dist')
