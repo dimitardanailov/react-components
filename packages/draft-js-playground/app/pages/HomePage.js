a48579eb539977dd07435fa48d14979aa83165a4
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-03 21:40:34 +0200

    HomePage updates the import statement.

--- packages/draft-js-playground/app/pages/HomePage.js
+++ packages/draft-js-playground/app/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import PlaygroundEditor from '../components/Editor'
+import PlaygroundEditor from '../components/PlaygroundEditor'
 
 function HomePage() {
   return (
     <div>
+      <h2>Hello home page ...</h2>
       <PlaygroundEditor />
     </div>
   )
