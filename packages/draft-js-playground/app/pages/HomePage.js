89a701a59b56402e396c51f3a4311575b1296bc2
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-04 16:31:47 +0200

    Home page is able to set EditorState via html markdown data.

--- packages/draft-js-playground/app/pages/HomePage.js
+++ packages/draft-js-playground/app/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { convertToRaw } from 'draft-js'
+import { convertToRaw, convertFromRaw, EditorState } from 'draft-js'
 
 import PlaygroundEditor from '../components/PlaygroundEditor'
 import convertFromHTMLToState from '../components/convertFromHTMLToState'
@@ -9,14 +9,15 @@ const imageURL =
   'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
 
 const sampleMarkup = `
-<b>Bold text</b>, <i>Italic text</i><br/ ><br />
-<a href="https://www.google.com/">Google.com</a><br /><br/ >
-<img src="${imageURL}" height="272" width="92" />
+  <b>Bold text</b>, <i>Italic text</i><br/ ><br />
+  <a href="https://www.google.com/">Google.com</a><br /><br/ >
+  <img src="${imageURL}" height="272" width="92" />
 `
 
 const editorState = convertFromHTMLToState(sampleMarkup)
 const contentState = editorState.getCurrentContent()
 const rawData = convertToRaw(contentState)
+const rawState = EditorState.createWithContent(convertFromRaw(rawData))
 
 const emptyEditorState = convertFromHTMLToState('')
 
@@ -37,7 +38,11 @@ function HomePage() {
         <code>{sampleMarkup}</code>
         <h3>Draftjs raw data equalent</h3>
         <code>{JSON.stringify(rawData)}</code>
-        <PlaygroundEditor placeholder="Simple placeholder" required />
+        <PlaygroundEditor
+          placeholder="Simple placeholder"
+          required
+          previousEditorState={rawState}
+        />
       </section>
     </section>
   )
