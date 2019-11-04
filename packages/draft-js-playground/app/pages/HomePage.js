ea279fedb4ebe9339594966e0d3d00ce729ac198
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-04 11:27:32 +0200

    Home page sets simple html markup.

--- packages/draft-js-playground/app/pages/HomePage.js
+++ packages/draft-js-playground/app/pages/HomePage.js
@@ -2,11 +2,24 @@ import React from 'react'
 
 import PlaygroundEditor from '../components/PlaygroundEditor'
 
+const imageURL =
+  'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+
+const sampleMarkup = `
+<b>Bold text</b>, <i>Italic text</i><br/ ><br />
+<a href="https://www.google.com/">Google.com</a><br /><br/ >
+<img src="${imageURL}" height="272" width="92" />
+`
+
 function HomePage() {
   return (
     <div>
-      <h2>Hello home page ...</h2>
-      <PlaygroundEditor placeholder="Simple placeholder" required />
+      <h2>Sample HTML converted into Draft content:</h2>
+      <PlaygroundEditor
+        placeholder="Simple placeholder"
+        required
+        fieldValue={sampleMarkup}
+      />
     </div>
   )
 }
