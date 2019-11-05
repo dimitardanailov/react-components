e9fd9bdc04ab05c53d35ac1831f507be0918853a
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-05 08:54:26 +0200

    Simple Link decator implementation.

---
+++ packages/draft-js-playground/app/components/playground-editor/decorators/Link.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function findLinkEntities(contentBlock, callback, contentState) {
+  contentBlock.findEntityRanges(character => {
+    const entityKey = character.getEntity()
+    return (
+      entityKey !== null &&
+      contentState.getEntity(entityKey).getType() === 'LINK'
+    )
+  }, callback)
+}
+
+const Link = props => <a href="">simple link ...</a>
+
+export { Link, findLinkEntities }
