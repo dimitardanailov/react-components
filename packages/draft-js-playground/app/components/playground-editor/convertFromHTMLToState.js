c84328b3bce4107396abc2695d07dd41f17a86c8
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-05 09:44:23 +0200

    convertFromHTMLToState has two additional functions: convertFromHTMLMarkup and convertFromHTMLToStateDecator.

--- packages/draft-js-playground/app/components/playground-editor/convertFromHTMLToState.js
+++ packages/draft-js-playground/app/components/playground-editor/convertFromHTMLToState.js
@@ -1,15 +1,31 @@
 import { EditorState, ContentState, convertFromHTML } from 'draft-js'
 
-export default function convertFromHTMLToState(markup) {
-  if (markup.length === 0) return EditorState.createEmpty()
-
+function convertFromHTMLMarkup(markup) {
   const blocksFromHTML = convertFromHTML(markup)
   const state = ContentState.createFromBlockArray(
     blocksFromHTML.contentBlocks,
     blocksFromHTML.entityMap
   )
 
+  return state
+}
+
+function convertFromHTMLToStateDecator(markup, decator) {
+  if (markup.length === 0) return EditorState.createEmpty()
+
+  const state = convertFromHTMLMarkup(markup)
+  const editorState = EditorState.createWithContent(state, decator)
+
+  return editorState
+}
+
+export default function convertFromHTMLToState(markup) {
+  if (markup.length === 0) return EditorState.createEmpty()
+
+  const state = convertFromHTMLMarkup(markup)
   const editorState = EditorState.createWithContent(state)
 
   return editorState
 }
+
+export { convertFromHTMLToStateDecator, convertFromHTML }
