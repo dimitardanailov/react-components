d609fcaf8e57d2026339ea37a1bd59c1d246f739
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-11-05 16:42:11 +0200

    Custom decator link

--- packages/draft-js-playground/app/components/playground-editor/decorators/Link.js
+++ packages/draft-js-playground/app/components/playground-editor/decorators/Link.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import { ContentState } from 'draft-js'
+
+import styled from 'styled-components'
+
+const CustomLink = styled.a`
+  font-weight: bold;
+  color: #cc0000;
+`
 
 function findLinkEntities(contentBlock, callback, contentState) {
   contentBlock.findEntityRanges(character => {
@@ -10,6 +20,16 @@ function findLinkEntities(contentBlock, callback, contentState) {
   }, callback)
 }
 
-const Link = props => <a href="">simple link ...</a>
+function Link({ contentState, entityKey, children }) {
+  const { url } = contentState.getEntity(entityKey).getData()
+
+  return <CustomLink href={url}>{children}</CustomLink>
+}
+
+Link.propTypes = {
+  contentState: PropTypes.instanceOf(ContentState),
+  entityKey: PropTypes.string,
+  children: PropTypes.array,
+}
 
 export { Link, findLinkEntities }
