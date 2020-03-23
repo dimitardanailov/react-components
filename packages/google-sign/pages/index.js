2341e5c42ec7fd68c65b396ea13df6083d2f716a
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-03-23 19:57:47 +0200

    Indexpage uses MainLayout.

--- packages/google-sign/pages/index.js
+++ packages/google-sign/pages/index.js
@@ -1,7 +1,7 @@
-export default function Index() {
-  return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-}
+import MainLayout from '../layouts/MainLayout'
+
+import GoogleSignIn from '../components/Google/SignIn'
+
+const Page = () => <GoogleSignIn />
+
+export default MainLayout(Page)
