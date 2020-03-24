c4754773e751a138abd2944d49d2e0d8b8535b3d
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-03-24 10:24:49 +0200

    Sigin in button has position center.

--- packages/google-sign/pages/index.js
+++ packages/google-sign/pages/index.js
@@ -2,6 +2,20 @@ import MainLayout from '../layouts/MainLayout'
 
 import GoogleSignIn from '../components/Google/SignIn'
 
-const Page = () => <GoogleSignIn />
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  position: relative;
+
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`
+
+const Page = () => (
+  <Wrapper>
+    <GoogleSignIn />
+  </Wrapper>
+)
 
 export default MainLayout(Page)
