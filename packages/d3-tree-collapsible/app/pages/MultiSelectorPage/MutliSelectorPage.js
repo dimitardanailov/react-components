cf1d1f195593acfb251f09f21d346292b94a1507
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-02-14 11:42:09 +0200

    The script is able to retrieve the correct tree node.

--- packages/d3-tree-collapsible/app/pages/MultiSelectorPage/MutliSelectorPage.js
+++ packages/d3-tree-collapsible/app/pages/MultiSelectorPage/MutliSelectorPage.js
@@ -1,15 +1,28 @@
 import { hot } from 'react-hot-loader/root'
 
 import nodes from './jsons/nodes.json'
-import data from './jsons/data.json'
+
+import foods from './jsons/treeNodes/food-and-drink.json'
+import animals from './jsons/treeNodes/animals.json'
 
 import D3TreeNodeSwitcher from '../../components/D3TreeNodeSwitcher'
 
 function MultiSelectorPage() {
-  const updateParentChildRelationship = id => {
-    console.log('id', id)
+  const identifiers = {
+    '6026e7b0397ee9002f080c1b': 'food-and-drink',
+    '6026e7b0397ee9002f080c1c': 'animals',
+  }
 
-    return data
+  const treeNodes = {
+    'food-and-drink': foods,
+    animals: animals,
+  }
+
+  const updateParentChildRelationship = id => {
+    const identifier = identifiers[id]
+    const treeData = treeNodes[identifier]
+
+    return treeData
   }
 
   const params = {
