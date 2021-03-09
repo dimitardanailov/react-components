ee60545d9f00e752948fd366194abc2d8590b73b
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-03-09 09:21:15 +0200

    sharable resource uses box shadow.

--- packages/d3-tree-collapsible/app/components/styled-components/sharable.js
+++ packages/d3-tree-collapsible/app/components/styled-components/sharable.js
@@ -1,10 +1,17 @@
 const ElementWrapper = window.styled.div`
   margin: 1rem;
+
+  border-radius: 4px;
+  box-shadow: 3px 3px 10px 0 rgba(0, 0, 0, 0.3);
+  border: 1px solid #b5b5b5;
+  background-color: #ffffff;
+
+  padding: 1rem;
 `
 const SVGContainer = window.styled.div`
   position: relative;
 
-  border: 2px solid #808080;
+  margin-top: 1rem;
 `
 
 const ParentContainer = window.styled.div`
