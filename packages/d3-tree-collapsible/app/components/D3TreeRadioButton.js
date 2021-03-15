3ac836b73a1ccf40b40251618da1d6d572de52e7
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-03-15 16:10:41 +0200

    Component is able to display nodeElements.

--- packages/d3-tree-collapsible/app/components/D3TreeRadioButton.js
+++ packages/d3-tree-collapsible/app/components/D3TreeRadioButton.js
@@ -1,13 +1,40 @@
 import { useMachine } from '@xstate/react'
+
+import createTreeNodeSwitcher from './machines/TreeNodeSwitcher'
+
+import SelectorListItem from './SelectorListItem'
+
 import {
   ElementWrapper,
   SVGContainer,
   ParentContainer,
+  StyledNodeContainer,
 } from './styled-components/sharable'
 
 // ============ D3TreeRadioButton ====================
-function D3TreeRadioButton() {
-  return React.createElement('div', null, `Hello D3TreeRadioButton`)
+function D3TreeRadioButton({ dbNodes }) {
+  const [nodes, setNodes] = React.useState(dbNodes)
+
+  const [stateSwitcher, sendSwitcher, serviceSwitcher] = useMachine(
+    createTreeNodeSwitcher(),
+  )
+
+  const stateSwitcherCallback = async node => {}
+
+  const nodeElements = nodes.map(node => {
+    return React.createElement(SelectorListItem, {
+      node,
+      stateSwitcherCallback,
+      key: node._id,
+      machine: null,
+    })
+  })
+
+  console.log('stateSwitcher', nodeElements)
+
+  const Wrapper = React.createElement(ElementWrapper, null, nodeElements)
+
+  return Wrapper
 }
 
 export default D3TreeRadioButton
