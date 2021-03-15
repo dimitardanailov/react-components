5bb1b51993d38a5544d5741c187404adaa1aae3d
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-03-15 16:11:02 +0200

    The element will be responsible to change active main node.

---
+++ packages/d3-tree-collapsible/app/components/SelectorListItem.js
@@ -0,0 +1,29 @@
+import {
+  StyledSelectorListItem,
+  StyledSelectorListTextItem,
+} from './styled-components/sharable'
+
+function SelectorListItem({ node, stateSwitcherCallback, machine }) {
+  const clickListener = () => {
+    stateSwitcherCallback(node)
+  }
+
+  const TextItem = React.createElement(
+    StyledSelectorListTextItem,
+    {
+      // checked: machine.state.context.activeNodeId === node._id,
+    },
+    node.name,
+  )
+
+  return React.createElement(
+    StyledSelectorListItem,
+    {
+      key: node._id,
+      onClick: clickListener,
+    },
+    TextItem,
+  )
+}
+
+export default SelectorListItem
