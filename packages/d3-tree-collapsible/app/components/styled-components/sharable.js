894f5320e696c485e240efa1dd7a419790485712
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-03-15 16:11:49 +0200

    sharable component have two additional components.

--- packages/d3-tree-collapsible/app/components/styled-components/sharable.js
+++ packages/d3-tree-collapsible/app/components/styled-components/sharable.js
@@ -23,4 +23,43 @@ const ParentContainer = window.styled.div`
   align-items: center;
 `
 
-export { ElementWrapper, SVGContainer, ParentContainer }
+const StyledSelectorListItem = window.styled.div`
+  position: relative;
+
+  padding: 0 0.35rem;
+  cursor: pointer;
+
+  text-align: center;
+`
+
+const StyledSelectorListTextItem = window.styled.div`
+  background: #fff;
+
+  padding: 4px 12px;
+  color: rgba(0, 0, 0, 0.8); 
+  background-color: ${props => (props.checked ? '#d9ddf5' : '#fff')}; 
+
+  margin-bottom: 0.4rem;
+
+  border-radius: 4px;
+  border: solid 1px #b5b5b5;
+
+  box-shadow: ${props =>
+    props.checked ? '3px 3px 10px 0 rgba(0, 0, 0, 0.3)' : 'none'}; ;
+
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+
+  text-transform: uppercase;
+`
+
+export {
+  ElementWrapper,
+  SVGContainer,
+  ParentContainer,
+  StyledSelectorListItem,
+  StyledSelectorListTextItem,
+}
