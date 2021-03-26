40dd4f3a88f03169a615763ab7bd9b248cb4bc4b
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-03-26 16:47:43 +0200

    Source code cleanup.

--- packages/d3-tree-collapsible/app/components/D3TreeRadioButton.js
+++ packages/d3-tree-collapsible/app/components/D3TreeRadioButton.js
@@ -254,7 +254,7 @@ function loadRadioButtonTree(
   root.y0 = 0
 
   const entityIsActive = (d, selectedEntity) => {
-    if (selectedEntity == null || !selectedEntity.length) return true
+    if (selectedEntity == null) return true
 
     return selectedEntity._id === d.data._id
   }
